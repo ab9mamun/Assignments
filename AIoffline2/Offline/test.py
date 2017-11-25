7b9f26ea38b08620abbe39514048e797ea8a22c9
Mamun <ab9mamun@gmail.com>
2017-11-26 02:36:04 +0600

    Bugfix

--- AIoffline2/Offline/test.py
+++ AIoffline2/Offline/test.py
@@ -1,55 +1,25 @@
-# import random
-# import urllib.request
-#
-# def download_web_image(url):
-#     name= random.randrange(1,1000)
-#     full_name = str(name)+".jpg"
-#     urllib.request.urlretrieve(url, full_name)
-#
-# url = "https://i.ytimg.com/vi/EfYoA36N9nM/hqdefault.jpg"
-# download_web_image(url)
 
 
-def initialize():
-    periods = 6
-    days = 5
-    weight_vector = (1,1,1)
-    fname = 'input.txt'
-    requirements = []
-    with open(fname) as f:
-        content = f.readlines()
-    # you may also want to remove whitespace characters like `\n` at the end of each line
-    content = [x.strip() for x in content]
-    f.close()
-    for line in content:
-        #   print(line)
-        words = line.split(" ")
-       # print(words)
-        if words[0] == "Timetable":
-            periods = int(words[2])
-            days = int(words[6])
-            #print(periods, days)
-        elif words[0] == 'Schedule':
-            cls = int(words[1][1:])
-            teacher = int(words[5][1:])
-            room = int(words[7][1:])
-            times = int(words[8])
-           # print(teacher, cls, room, times)
-            for i in range(times):
-                requirements.append((teacher, cls, room))
-        elif words[0] == 'Importance':
-            metric = words[2]
-            weight = int(words[4])
-            if metric == 'Teacherclashes':
-                weight_vector = (weight, weight_vector[1],weight_vector[2])
-            elif metric == 'Classclashes':
-                weight_vector = (weight_vector[0], weight,weight_vector[2])
-            elif metric == 'Roomclashes':
-                weight_vector = (weight_vector[0], weight_vector[1], weight)
+def cost_func(state):
+    cost = 0
+    for slot in state:
+        teacher_count = [0] * (10+1)
+        class_count = [0] * (10+1)
+        room_count = [0] * (10+1)
 
-    return periods, days, weight_vector, requirements
+        for req in slot:
+            teacher_count[req[0]] = teacher_count[req[0]]+1
+            class_count[req[1]] = class_count[req[1]]+1
+            room_count[req[2]] = room_count[req[2]]+1
+        for count in teacher_count:
+            cost = cost + max(count - 1, 0)*1
+        for count in class_count:
+            cost = cost + max(count - 1, 0)*1
+        for count in room_count:
+            cost = cost + max(count - 1, 0)*1
 
-periods, days, weight_vector, requirements = initialize()
+    return cost
 
-print(periods, days, weight_vector, len(requirements))
-print(requirements)
+
+state = [[(3, 1, 2)], [(4, 3, 1), (4, 1, 4), (2, 1, 3), (2, 1, 4)], [(3, 2, 3)], [(4, 2, 4), (1, 3, 4), (2, 3, 2)], [(3, 4, 1)], [(3, 4, 3)], [(1, 4, 1), (4, 1, 3), (4, 3, 4), (2, 4, 1)], [(4, 4, 2), (3, 3, 2), (1, 4, 4), (4, 2, 4), (2, 4, 3), (1, 1, 4), (4, 1, 2)], [(4, 1, 1), (2, 4, 2)], [(1, 4, 3), (4, 2, 2)], [(2, 1, 1), (1, 3, 3)], [(4, 4, 1), (3, 2, 2)], [(2, 1, 2), (1, 4, 3), (1, 3, 1), (2, 2, 4), (3, 2, 4)], [(2, 2, 2)], [(1, 3, 2), (4, 3, 1), (2, 4, 1), (1, 1, 3), (3, 4, 1), (4, 3, 1), (2, 3, 1)], [(3, 2, 3), (4, 4, 2)], [(3, 3, 4)], [(1, 1, 4)], [(4, 2, 1), (4, 3, 1)], [(1, 3, 4), (4, 4, 4), (2, 3, 3), (4, 4, 1)], [(1, 4, 3)], [(3, 3, 4), (4, 2, 4), (3, 4, 4), (4, 2, 3)], [(2, 2, 1), (3, 2, 3), (4, 2, 2), (3, 3, 1), (1, 4, 2)], [(3, 2, 3), (2, 3, 4), (1, 1, 2), (4, 3, 1)], [(2, 3, 4), (1, 4, 3), (2, 1, 2)], [(2, 3, 4), (2, 1, 1)], [(1, 4, 4)], [(1, 1, 1), (4, 1, 2), (2, 2, 2), (3, 1, 1)], [(3, 2, 2), (1, 4, 3)], [(3, 2, 1), (1, 3, 4)], [(2, 1, 2), (2, 2, 2), (3, 4, 2), (1, 2, 4)], [(1, 4, 1), (4, 2, 4), (2, 1, 2), (3, 1, 4), (3, 3, 3), (4, 1, 3), (3, 4, 3), (2, 2, 4)], [(3, 4, 1), (1, 1, 1), (3, 3, 3), (2, 2, 4), (2, 2, 4), (4, 1, 1)], [(1, 4, 3), (4, 2, 1)], [(3, 3, 3), (1, 1, 4), (3, 1, 3), (3, 3, 3), (2, 2, 2)], [(2, 4, 2)], [(1, 1, 2), (3, 1, 4), (3, 2, 3), (4, 3, 3)], [(1, 4, 2), (1, 2, 1), (2, 1, 2), (1, 3, 3)], [(2, 3, 2)], [(1, 1, 3), (4, 4, 3), (4, 3, 1), (3, 2, 2)]]
+print(cost_func(state))
