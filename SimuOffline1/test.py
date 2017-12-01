a8e8f4561df9d08642ab06d655c1d95cd504639d
Mamun <ab9mamun@gmail.com>
2017-12-01 21:16:17 +0600

    Working on simu offline 2

---
+++ SimuOffline1/test.py
@@ -0,0 +1,21 @@
+class Point:
+    def __init__(self, x = 0, y = 0):
+        self.x = x
+        self.y = y
+
+    def get(self):
+        return self.x, self.y
+
+
+def main():
+   # p = Point()
+   # p2 = Point(2,7)
+   # print(p.get())
+   # print(p2.get())
+   for foo in range(10):
+       bar = 2
+   print(foo, bar)
+
+
+if __name__ == "__main__":
+    main()
