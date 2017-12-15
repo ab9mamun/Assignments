4de042d71ba0e63aef75d19d162190805a62d59e
Abdullah Al Mamun <ab9mamun@gmail.com>
2017-12-15 20:48:57 +0600

    created variable.sh

---
+++ variables.sh
@@ -0,0 +1,2 @@
+my_javac="/home/abdullahm/_git/mmtk/openjdk/build/linux-x86_64-normal-server-release/jdk/bin/javac"
+my_java="/home/abdullahm/_git/mmtk/openjdk/build/linux-x86_64-normal-server-release/jdk/bin/java"
